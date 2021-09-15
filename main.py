fe7ef7f1017e34ff52429cc0c1fc95f65d393da5
parag1304 <71979018+parag1304@users.noreply.github.com>
2021-09-15 14:48:30 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+from Generator import generator
+Defchar = 'abcdefghijklmnopqrstuvwxyz1234567890@#$%'
+print("Default charset ='abcdefghijklmnopqrstuvwxyz1234567890@#$%' ")
+Deflength = int(input('Enter password length \n'))
+generator(Deflength, Defchar)
